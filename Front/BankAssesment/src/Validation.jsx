26f0cd9046ef3b6e196d854ac16575a311468d60
NataliaPobleteIllescasSolera <138590736+NataliaPobleteIllescasSolera@users.noreply.github.com>
2023-07-05 14:02:16 +0200

    Changes in the register
    I do the register form and the validation in the front

---
+++ Front/BankAssesment/src/Validation.jsx
@@ -0,0 +1,36 @@
+const Validation = (values) => {
+    let errors = {};
+
+    if (!values.firstname) {
+        errors.firstname = 'First name is required';
+    }
+
+    if (!values.lastname) {
+        errors.lastname = 'Last name is required';
+    }
+
+    if (!values.username) {
+        errors.username = 'Username is required';
+    } else if(values.username != "solera@solera.com"){
+        errors.username = 'Invalid username';
+    }
+
+    if (!values.password) {
+        errors.password = 'Password is required';
+    } else if(values.password != "bootcamp5"){
+        errors.password = 'Invalid password';
+    }
+
+    if (values.password != values.confirmpassword) {
+        isValid = false;
+        errors.pass = "Passwords don't match.";
+    }else if(!values.password || !values.confirmpassword){
+        errors.password = 'Password is required';
+    }
+      
+    return errors;
+}
+export default Validation;
+
+
+

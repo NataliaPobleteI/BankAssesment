bbfe3a6a288f9b97783b5d70cb827c1851a65f0d
Irene Godoy <irene.godoy@solera.com>
2023-07-05 10:33:49 +0200

    Add TopNavbar

---
+++ Front/BankAssesment/src/TopNavbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+// import { Link } from 'react-router-dom'
+ import "./TopNavbar.css"
+
+const TopNavbar = () => {
+
+
+
+
+
+
+  return (
+    <div className="navbar ">
+      <div >
+        
+      </div>
+        <div >
+          <h1 className="elh1">Real World App</h1>
+        </div>
+        
+        <div >
+         <button id='newButton'>New</button>
+        </div>
+      
+    </div>
+  )
+}
+
+export { TopNavbar }
